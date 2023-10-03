d2d3eede0bca6ee9d6e17200649323fd1ff5fdb9
Aminah Shahzad <135119713+ameynahfr@users.noreply.github.com>
2023-10-03 19:48:46 +0500

    crud user data

--- controllers/userController.js
+++ controllers/userController.js
@@ -124,7 +124,7 @@ export const searchUserByUniqueFields = async (req, res) => {
   
       return res.status(200).json({ message: "User successfully deleted" });
     } catch (error) {
-      res.status(404).json({
+      res.status(500).json({
         message: error.message,
       });
     }
